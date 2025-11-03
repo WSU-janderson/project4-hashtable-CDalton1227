0985d105bb78746b10b766cbce2a392b6229e222
chris <dalton.99@wright.edu>
2025-11-03 17:10:15 -0500

    started on HashTable.cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,3 +1,12 @@
 /**
  * HashTable.cpp
  */
+
+#include "HashTable.h"
+
+HashTable::HashTable(size_t initCapacity) {
+    trueSize = 0; // nothing in it yet
+    capacity = initCapacity; // set to the size input in constructor
+
+    buckets.resize(capacity); // resize (vector) to initCapacity number of buckets
+}
