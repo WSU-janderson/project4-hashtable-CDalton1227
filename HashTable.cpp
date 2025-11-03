093f3d2783223f49787bdabd17aae87ac4ec24a5
chris <dalton.99@wright.edu>
2025-11-03 17:44:27 -0500

    fixed formatting, added hash function, added friend access

--- HashTable.cpp
+++ HashTable.cpp
@@ -3,10 +3,17 @@
  */
 
 #include "HashTable.h"
+#include <iostream>
+#include <functional>
 
 HashTable::HashTable(size_t initCapacity) {
     trueSize = 0; // nothing in it yet
     capacity = initCapacity; // set to the size input in constructor
 
     buckets.resize(capacity); // resize (vector) to initCapacity number of buckets
+}
+
+size_t HashTable::hash(const std::string& key) const {
+    size_t hashVal = std::hash<std::string>()(key);
+    return hashVal % capacity; // keep index within bounds
 }
--- HashTable.h
+++ HashTable.h
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <optional>
+#include <ostream>
 
 // Create an enum for the bucket type
 // NORMAL - not empty,
@@ -17,26 +19,30 @@ enum class BucketType {
 
 // create the hash table buckets
 class HashTableBucket {
-public:
-    HashTableBucket () {
-        type = BucketType::ESS; // default type
-    }
-private:
-    std::string key;
-    size_t value;
-    BucketType type;
-};
+    friend class HashTable;
+    public:
+        HashTableBucket () {
+            type = BucketType::ESS; // default type
+        }
+    private:
+        std::string key;
+        size_t value;
+        BucketType type;
+    };
 
 // create the hash table class
 class HashTable {
     public:
         // constructor that sets size; default 8
         HashTable(size_t initCapacity = 8);
+        friend std::ostream& operator<<(std::ostream& os, const HashTable& ht);
 
     private:
         std::vector<HashTableBucket> buckets;
         size_t trueSize; // number of things in it
         size_t capacity; // number of things it could have
         std::vector<size_t> offsets; // probing offsets
+
+        size_t hash(const std::string& key) const; // hash function to prevent excessive repetition in every other method
 };
 
