ecfab63431c90039d24fd10e8388b0762a59b5f7
chris <dalton.99@wright.edu>
2025-11-03 17:56:43 -0500

    probing offsets and shuffling

--- HashTable.cpp
+++ HashTable.cpp
@@ -3,14 +3,23 @@
  */
 
 #include "HashTable.h"
-#include <iostream>
 #include <functional>
+#include <numeric>
+#include <random>
+#include <algorithm>
 
 HashTable::HashTable(size_t initCapacity) {
     trueSize = 0; // nothing in it yet
     capacity = initCapacity; // set to the size input in constructor
 
     buckets.resize(capacity); // resize (vector) to initCapacity number of buckets
+
+    offsets.resize(capacity - 1);
+
+    std::iota(offsets.begin(), offsets.end(), 1); // make the list of offsets starting with 1
+    std::random_device rd; // seed for rng
+    std::mt19937 gen(rd()); // create rng
+    std::shuffle(offsets.begin(), offsets.end(), gen); // shuffle list
 }
 
 size_t HashTable::hash(const std::string& key) const {
