be4c0761f20a34cd52be09062940f9fb76fecd89
CDalton1227 <144410090+CDalton1227@users.noreply.github.com>
2025-11-03 16:45:53 -0500

    Update HashTable.h
    started on classes: buckettype hashtablebucket and hashtable

--- HashTable.h
+++ HashTable.h
@@ -1,3 +1,36 @@
 /**
  * HashTable.h
  */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Create an enum for the bucket type
+// NORMAL - not empty,
+// ESS - empty since start,
+// EAR - empty after removal
+enum class BucketType {
+    NORMAL, ESS, EAR
+};
+
+
+// create the hash table buckets
+class HashTableBucket {
+public:
+    HashTableBucket () {
+        type = BucketType::ESS; // default type
+    }
+private:
+    std::string key;
+    size_t value;
+    BucketType type;
+};
+
+// create the hash table class
+class HashTable {
+    public:
+    private:
+        std::vector<HashTableBucket> buckets;
+};
+
